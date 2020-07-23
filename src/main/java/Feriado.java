d6b3bd60bcf3e3329bdf53714b72e0ad7bde4b92
diegosg1989 <diego.sguerra@gmail.com>
2020-07-23 20:42:49 -0300

    alguns ajustes

---
+++ src/main/java/Feriado.java
@@ -0,0 +1,55 @@
+import java.util.List;
+
+/**
+ * Classe que auxilia na desserialização do json para consulta de feriados
+ * @author diegosg
+ *
+ */
+public class Feriado {
+
+	private List<Holiday> Holiday;
+
+	public Feriado() {
+	}
+
+	public List<Holiday> getHoliday() {
+		return Holiday;
+	}
+
+	@Override
+	public String toString() {
+		return "Feriado [Holiday=" + Holiday + "]";
+	}
+}
+
+class Holiday {
+
+	private String Type;
+	private String Description;
+	private String Date;
+	private String Name;
+
+	public Holiday() {
+	}
+
+	public String getType() {
+		return Type;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public String getDate() {
+		return Date;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	@Override
+	public String toString() {
+		return "Holiday [Type=" + Type + ", Description=" + Description + ", Date=" + Date + ", Name=" + Name + "]";
+	}
+}
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+
+import org.json.JSONObject;
+import org.json.XML;
 
 import com.google.gson.GsonBuilder;
 
@@ -23,22 +27,62 @@ public class Utils {
 	 */
 	public static Endereco buscaEnderecoPorCEP(String cep) {
 
+		String urlString = String.format("http://viacep.com.br/ws/%s/json", cep);
+		
 		try {
 
-			URL url = new URL(String.format("http://viacep.com.br/ws/%s/json", cep));
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			String jsonString = buscaUrl(urlString);
 
-			StringBuilder jsonSb = new StringBuilder();
-
-			in.lines().forEach(l -> jsonSb.append(l.trim()));
-
-			in.close();
-
-			return new GsonBuilder().create().fromJson(jsonSb.toString(), Endereco.class);
+			return new GsonBuilder().create().fromJson(jsonString, Endereco.class);
 
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	/**
+	 * Dado um determinado ano, faz a buscas dos {@link Feriado}
+	 * @param ano
+	 * @return
+	 */
+	public static Feriado buscaFeriadosPorAno(int ano) {
+		
+		String urlString = String.format("http://services.sapo.pt/Holiday/GetNationalHolidays?year=%s", ano);
+		
+		try {
+			
+			String xmlString = buscaUrl(urlString);
+			
+			JSONObject paisesJson = XML.toJSONObject(xmlString);			
+			JSONObject getNationalHolidaysResponse = (JSONObject) paisesJson.get("GetNationalHolidaysResponse");
+			Object getNationalHolidaysResult = getNationalHolidaysResponse.get("GetNationalHolidaysResult");
+			
+			return new GsonBuilder().create().fromJson(getNationalHolidaysResult.toString(), Feriado.class);
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Dado uma determinada url, faz a busca na api e retorna uma string com valores(json ou xml)
+	 * @param urlString
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	private static String buscaUrl(String urlString) throws MalformedURLException, IOException {
+		
+		URL url = new URL(urlString);
+		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+		StringBuilder string = new StringBuilder();
+
+		in.lines().forEach(l -> string.append(l.trim()));
+		
+		in.close();
+		
+		return string.toString();
 	}
 	
 	/**
@@ -66,4 +110,17 @@ public class Utils {
 		
 		return true;
 	}
+	
+	
+	public static void main(String[] args) {
+		
+		Feriado buscaFeriadosPorAno = buscaFeriadosPorAno(2020);
+		Endereco buscaEnderecoPorCEP = buscaEnderecoPorCEP("04842050");
+		
+		System.out.println(buscaFeriadosPorAno);
+		System.out.println(buscaEnderecoPorCEP);
+		
+	}
 }
+
+
