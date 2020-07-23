0fe9d26f8dc6547afcd92f70c3abcc7556ff5d87
diegosg1989 <diego.sguerra@gmail.com>
2020-07-23 12:18:44 -0300

    algumas alterações

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -2,23 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 
 import com.google.gson.GsonBuilder;
 
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CPFValidator;
+
 /**
- * Classe utilitaria que fará algumas buscas por APIs  
+ * Classe utilitaria que fará algumas buscas por APIs
  */
 public class Utils {
-	
-	public static void main(String[] args) throws IOException{
-		
-		Endereco buscaEnderecoPorCEP = buscaEnderecoPorCEP("04842050");
-		
-		System.out.println(buscaEnderecoPorCEP);
-	}
 
 	/**
 	 * Dado um determinado cep, faz a busca do {@link Endereco}
+	 * 
 	 * @param cep
 	 * @return {@link Endereco}
 	 * @throws IOException
@@ -26,7 +24,7 @@ public class Utils {
 	public static Endereco buscaEnderecoPorCEP(String cep) {
 
 		try {
-			
+
 			URL url = new URL(String.format("http://viacep.com.br/ws/%s/json", cep));
 			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
@@ -35,12 +33,37 @@ public class Utils {
 			in.lines().forEach(l -> jsonSb.append(l.trim()));
 
 			in.close();
-			
+
 			return new GsonBuilder().create().fromJson(jsonSb.toString(), Endereco.class);
 
-		} 
-		catch (Exception e) {
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	/**
+	 * Dado um determinado cep, faz a validação
+	 * @param cep
+	 * @return
+	 */
+	public static boolean validaCEP(String cep) {
+		String padrao = "\\d{5}\\d{3}";
+		return cep.matches(padrao);
+	}
+
+	/**
+	 * Dado um determinado cpf, faz a validação dele
+	 * @param cpf
+	 * @return true valido, false invalido
+	 */
+	public static boolean validaCPF(String cpf) {
+		
+		CPFValidator cpfValidator = new CPFValidator(); 
+		List<ValidationMessage> erros = cpfValidator.invalidMessagesFor(cpf); 
+		
+		if(erros.size() > 0) 
+			return false;
+		
+		return true;
 	}
 }
