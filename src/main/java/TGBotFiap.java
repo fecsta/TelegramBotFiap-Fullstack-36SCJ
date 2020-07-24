c8a15348ce40e77f0e0737291074d010bc762f85
Felipe Costa <felipe.caraujo@telefonica.com>
2020-07-24 12:28:40 -0300

    Merge previsão do tempo - Douglas

--- src/main/java/TGBotFiap.java
+++ src/main/java/TGBotFiap.java
@@ -13,7 +13,7 @@ public class TGBotFiap extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update){
 
         ci.setChatId(update.getMessage().getChatId());
-        
+
         //Pega a mensagem inicial
         SendMessage message = new SendMessage()
                 .setChatId(update.getMessage().getChatId())
@@ -22,7 +22,7 @@ public class TGBotFiap extends TelegramLongPollingBot {
         //Se o  estado do objetivo estiver null, identifica como primeira interação
         if (update.hasMessage() && update.getMessage().hasText()&& ci.getState() == null) {
 
-            message.setText("Bem vindo ao TelegramBot Fiap - Curso FullStack");
+            message.setText("Bem vindo ao TelegramBot Fiap - MBA FullStack");
 
             try {
                 execute(message);
@@ -31,31 +31,31 @@ public class TGBotFiap extends TelegramLongPollingBot {
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
-            message.setText("Digite 'cep' para fazer uma busca de endereço por cep \nDigite 'finalizar' para encerrar o chat atual");
+            message.setText("Digite 'cep' para fazer uma busca de endereço por cep \nDigite 'previsão' para a previsão do tempo\nDigite 'finalizar' para encerrar o chat atual");
 
             try {
                 execute(message);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
-        //Caso tenha digitado o 'cep', será feito a solicitação do CEP e depois seta o estado ConsultarCEP para realizar a consulta
+            //Caso tenha digitado o 'cep', será feito a solicitação do CEP e depois seta o estado ConsultarCEP para realizar a consulta
         }else if (ci.getState().equals("ConsultarCEP")||(message.getText().equalsIgnoreCase("cep"))) {
 
-             if(ci.getState().equals("ConsultarCEP")){
-                 //Valida se foi digitado um cep antes de executar o serviço
-                 if (message.getText().length() < 8 || message.getText().length() > 8 ){
-                     message.setText("Não foi digitado um CEP, por favor, digite um cep corretamente");
-                     try {
-                         execute(message);
-                     } catch (TelegramApiException e) {
-                         e.printStackTrace();
-                     }
-                     return;
-                 }
-                 
+            if(ci.getState().equals("ConsultarCEP")){
+                //Valida se foi digitado um cep antes de executar o serviço
+                if (message.getText().length() < 8 || message.getText().length() > 8 ){
+                    message.setText("Não foi digitado um CEP, por favor, digite um cep corretamente");
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    return;
+                }
+
                 Endereco endereco = Utils.buscaEnderecoPorCEP(message.getText());
-                
-                
+
+
                 if(endereco == null) {
                     message.setText("O cep digitado não é valido, digite novamente");
                     try {
@@ -64,26 +64,23 @@ public class TGBotFiap extends TelegramLongPollingBot {
                         e.printStackTrace();
                     }
                 }else{
-                	
-                	StringBuilder enderecoString = new StringBuilder();
-                	enderecoString.append("Endereço: ").append(endereco.getLogradouro());
-                	enderecoString.append("\nBairro: ").append(endereco.getBairro());
-                	enderecoString.append("\nCidade: ").append(endereco.getLocalidade());
-                	enderecoString.append("\nEstado: ").append(endereco.getUf());
-                	
+
+                    StringBuilder enderecoString = new StringBuilder();
+                    enderecoString.append("Endereço: ").append(endereco.getLogradouro());
+                    enderecoString.append("\nBairro: ").append(endereco.getBairro());
+                    enderecoString.append("\nCidade: ").append(endereco.getLocalidade());
+                    enderecoString.append("\nEstado: ").append(endereco.getUf());
+
                     message.setText(enderecoString.toString());
-                    
+
                     ci.setState("MenuIni");
-                    
+
                     try {
                         execute(message);
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
                 }
-                
-                
-                
             }else{
                 message.setText("Digite o CEP a ser  consultado com 8 digitos");
                 ci.setState("ConsultarCEP");
@@ -93,7 +90,62 @@ public class TGBotFiap extends TelegramLongPollingBot {
                     e.printStackTrace();
                 }
             }
-        //Encerra o chat setando o estado como null
+            //Caso tenha digitado o 'previsão', será feito a solicitação da previsão do tempo e depois setado o estado PrevisaoTempo para realizar a consulta
+        }else if (ci.getState().equals("PrevisaoTempo")||(message.getText().equalsIgnoreCase("previsão"))) {
+
+            if(ci.getState().equals("PrevisaoTempo")){
+                //Verifica a validade do nome de cidade antes de executar
+                if (message.getText().length() < 3){
+                    message.setText("Cidade não existente, digite um nome válido");
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    return;
+                }
+
+                Tempo tempo = Utils.buscaPrevisaoTempoCidade(message.getText());
+
+                if(tempo == null) {
+                    message.setText("A cidade digitada não existe, tente novamente");
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+
+                    StringBuilder tempoString = new StringBuilder();
+                    tempoString.append("A previsão do tempo para: ").append(tempo.getName());
+                    tempo.getMain();
+                    tempoString.append("\nTemperatura: ").append(tempo.getMain().getTemp() + " °C");
+                    tempoString.append("\nSensação Térmica: ").append(tempo.getMain().getFeels_like() +" °C");
+                    tempoString.append("\nMáxima: ").append(tempo.getMain().getTemp_max() +" °C");
+                    tempoString.append("\nMínima: ").append(tempo.getMain().getTemp_min() +" °C");
+                    tempoString.append("\nUmidade do Ar: ").append(tempo.getMain().getHumidity()+" %");
+                    tempoString.append("\nVelocidade do Vento: ").append(tempo.getWind());
+
+                    message.setText(tempoString.toString());
+
+                    ci.setState("MenuIni");
+
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }else{
+                message.setText("Digite a cidade para qual deseja a previsão do tempo");
+                ci.setState("PrevisaoTempo");
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+            //Encerra o chat setando o estado como null
         }else if (message.getText().equalsIgnoreCase("finalizar")) {
             ci.setState(null);
             message.setText("Até Logo");
@@ -103,7 +155,7 @@ public class TGBotFiap extends TelegramLongPollingBot {
                 e.printStackTrace();
             }
         }else{
-            message.setText("Não entendi\nDigite a opção anterior ou digite 'finalizar' para encerrar");
+            message.setText("Não entendi\nDigite uma opção válida ou digite 'finalizar' para encerrar");
             try {
                 execute(message);
             } catch (TelegramApiException e) {
@@ -112,7 +164,7 @@ public class TGBotFiap extends TelegramLongPollingBot {
         }
         //Caso tenha saido de uma interação, repete o Menu para o usuário
         if (ci.getState().equals("MenuIni")){
-            message.setText("Digite 'cep' para fazer uma busca de endereço por cep \nDigite 'finalizar' para encerrar o chat atual");
+            message.setText("Digite 'cep' para fazer uma busca de endereço por cep \nDigite 'previsão' para a previsão do tempo\nDigite 'finalizar' para encerrar o chat atual");
             ci.setState("Menu");
             try {
                 execute(message);
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -4,14 +4,13 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
-
 import org.json.JSONObject;
 import org.json.XML;
 
 import com.google.gson.GsonBuilder;
-
 import br.com.caelum.stella.ValidationMessage;
 import br.com.caelum.stella.validation.CPFValidator;
+
 
 /**
  * Classe utilitaria que fará algumas buscas por APIs
@@ -19,51 +18,45 @@ import br.com.caelum.stella.validation.CPFValidator;
 public class Utils {
 
 	/**
+	 * Fornecida a cidade, faz a busca da Previsão do Tempo}
+	 * @param cidade
+	 * @return Previsão do Tempo}
+	 * @throws IOException
+	 */
+	public static Tempo buscaPrevisaoTempoCidade(String cidade) {
+
+		try {
+			URL urlTempo = new URL(String.format("http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=4a601093649a88540fad287e496bfc07&units=metric"));
+			BufferedReader brd = new BufferedReader(new InputStreamReader(urlTempo.openStream()));
+			StringBuilder jsonSt = new StringBuilder();
+			String line;
+			while((line = brd.readLine()) != null) {
+				jsonSt.append(line);
+			}
+			brd.close();
+			return new GsonBuilder().create().fromJson(jsonSt.toString(), Tempo.class);
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	/**
 	 * Dado um determinado cep, faz a busca do {@link Endereco}
-	 * 
+	 *
 	 * @param cep
 	 * @return {@link Endereco}
 	 * @throws IOException
 	 */
 	public static Endereco buscaEnderecoPorCEP(String cep) {
-
 		String urlString = String.format("http://viacep.com.br/ws/%s/json", cep);
-		
-		try {
 
+		try {
 			String jsonString = buscaUrl(urlString);
-
 			return new GsonBuilder().create().fromJson(jsonString, Endereco.class);
-
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	/**
-	 * Dado um determinado ano, faz a buscas dos {@link Feriado}
-	 * @param ano
-	 * @return
-	 */
-	public static Feriado buscaFeriadosPorAno(int ano) {
-		
-		String urlString = String.format("http://services.sapo.pt/Holiday/GetNationalHolidays?year=%s", ano);
-		
-		try {
-			
-			String xmlString = buscaUrl(urlString);
-			
-			JSONObject paisesJson = XML.toJSONObject(xmlString);			
-			JSONObject getNationalHolidaysResponse = (JSONObject) paisesJson.get("GetNationalHolidaysResponse");
-			Object getNationalHolidaysResult = getNationalHolidaysResponse.get("GetNationalHolidaysResult");
-			
-			return new GsonBuilder().create().fromJson(getNationalHolidaysResult.toString(), Feriado.class);
-			
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 	/**
 	 * Dado uma determinada url, faz a busca na api e retorna uma string com valores(json ou xml)
 	 * @param urlString
@@ -72,19 +65,39 @@ public class Utils {
 	 * @throws IOException
 	 */
 	private static String buscaUrl(String urlString) throws MalformedURLException, IOException {
-		
+
 		URL url = new URL(urlString);
 		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-
 		StringBuilder string = new StringBuilder();
-
 		in.lines().forEach(l -> string.append(l.trim()));
-		
+
 		in.close();
-		
+
 		return string.toString();
 	}
-	
+	/**
+	 * Dado um determinado ano, faz a buscas dos {@link Feriado}
+	 * @param ano
+	 * @return
+	 */
+	public static Feriado buscaFeriadosPorAno(int ano) {
+
+		String urlString = String.format("http://services.sapo.pt/Holiday/GetNationalHolidays?year=%s", ano);
+
+		try {
+
+			String xmlString = buscaUrl(urlString);
+
+			JSONObject paisesJson = XML.toJSONObject(xmlString);
+			JSONObject getNationalHolidaysResponse = (JSONObject) paisesJson.get("GetNationalHolidaysResponse");
+			Object getNationalHolidaysResult = getNationalHolidaysResponse.get("GetNationalHolidaysResult");
+
+			return new GsonBuilder().create().fromJson(getNationalHolidaysResult.toString(), Feriado.class);
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 	/**
 	 * Dado um determinado cep, faz a validação
 	 * @param cep
@@ -94,33 +107,37 @@ public class Utils {
 		String padrao = "\\d{5}\\d{3}";
 		return cep.matches(padrao);
 	}
-
 	/**
 	 * Dado um determinado cpf, faz a validação dele
 	 * @param cpf
 	 * @return true valido, false invalido
 	 */
+	// public static boolean validaCPF(String cpf) {
 	public static boolean validaCPF(String cpf) {
-		
-		CPFValidator cpfValidator = new CPFValidator(); 
-		List<ValidationMessage> erros = cpfValidator.invalidMessagesFor(cpf); 
-		
-		if(erros.size() > 0) 
+
+		// 	CPFValidator cpfValidator = new CPFValidator();
+		// 	List<ValidationMessage> erros = cpfValidator.invalidMessagesFor(cpf);
+		CPFValidator cpfValidator = new CPFValidator();
+		List<ValidationMessage> erros = cpfValidator.invalidMessagesFor(cpf);
+
+		// 	if(erros.size() > 0)
+		// 		return false;
+		if(erros.size() > 0)
 			return false;
-		
+
+		// 	return true;
+		// }
 		return true;
 	}
-	
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws IOException{
+
+		Tempo buscaPrevisaoTempoCidade = buscaPrevisaoTempoCidade("Paris");
+		Endereco buscaEnderecoPorCep = buscaEnderecoPorCEP("04671010");
 		Feriado buscaFeriadosPorAno = buscaFeriadosPorAno(2020);
-		Endereco buscaEnderecoPorCEP = buscaEnderecoPorCEP("04842050");
-		
+
+		System.out.println(buscaPrevisaoTempoCidade);
+		System.out.println(buscaEnderecoPorCep);
 		System.out.println(buscaFeriadosPorAno);
-		System.out.println(buscaEnderecoPorCEP);
-		
 	}
 }
-
-
