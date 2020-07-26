32282816444ff0cae9014e6360e5a7b1fb7ac559
System Administrator <root@MacBook-Air-de-Douglas.local>
2020-07-26 12:04:59 -0300

    merge com a versão mais atualizada do Diego

--- src/main/java/Feriado.java
+++ src/main/java/Feriado.java
@@ -1,13 +1,19 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Classe que auxilia na desserialização do json para consulta de feriados
- * @author diegosg
- *
  */
 public class Feriado {
 
 	private List<Holiday> Holiday;
+	private Map<String, Holiday> mapaDataFeriado = new HashMap<String, Holiday>();
 
 	public Feriado() {
 	}
@@ -19,6 +25,11 @@ public class Feriado {
 	@Override
 	public String toString() {
 		return "Feriado [Holiday=" + Holiday + "]";
+	}
+	
+	public Holiday buscaNoMapaPorData(String data) {
+		mapaDataFeriado = this.getHoliday().stream().collect(Collectors.toMap(h -> h.getDateFormatado(), h -> h));
+		return mapaDataFeriado.get(data);
 	}
 }
 
@@ -47,7 +58,29 @@ class Holiday {
 	public String getName() {
 		return Name;
 	}
-
+	
+	public String getDateFormatado() {
+		try {
+			DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Date date = (Date)formatter.parse(Date.replace("T", " "));
+			
+			return new SimpleDateFormat("dd/MM/yyyy").format(date);
+		}
+		catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public String getHolidayFormatado() {
+		
+		StringBuilder holiday = new StringBuilder();
+		holiday.append("Dia: ").append(this.getDateFormatado());
+		holiday.append("Tipo feriado: ").append(this.getType());
+		holiday.append("Nome: ").append(this.getName());
+		
+		return holiday.toString();
+	}
+	
 	@Override
 	public String toString() {
 		return "Holiday [Type=" + Type + ", Description=" + Description + ", Date=" + Date + ", Name=" + Name + "]";
